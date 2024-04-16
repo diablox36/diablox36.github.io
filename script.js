55e8b250a8d9ee1cb62cc362b14f76760bcdd7a4
diablox36 <thomas.rivest36@gmail.com>
2024-04-15 20:54:35 -0400

    draws the circles pretty good, still missing the lines

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 const h1 = document.querySelector('h1')
+const LINECOLOR = 'black'
+const FILLCOLOR = 'whitesmoke'
+const LINEWIDTH = 6
+const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink', 'brown', 'grey', 'black']
+
 let mouseOutInterval
 let checkforNewWindowInterval
 let positionX = window.screenX
@@ -16,15 +21,16 @@ if(checkCookie('index')) {
   index = 0
 }
 setCookie('index', index)
-resizeCanvas()
+redrawCanvas()
 
 h1.textContent = index
 
 checkforNewWindowInterval = setInterval(function () {
   getAllOtherWindows()
   if(oldWindows != windows.toString()) {
-    console.log('Other windows at : ' + windows)
+    console.log(windows)
     oldWindows = windows.toString()
+    redrawCanvas()
   }
 }, 1000)
 
@@ -32,7 +38,7 @@ window.onbeforeunload = function () {
   deleteCookie(index)
   index = nextIndex()
   setCookie('index',index)
-  return null;
+  return null
 }
 
 function setCookie(cookieName, cookieValue) {
@@ -61,7 +67,7 @@ function checkCookie(cookieName) {
 
 function deleteCookie(cookieName) {
   if (checkCookie(cookieName)) {
-    document.cookie = cookieName + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+    document.cookie = cookieName + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT"
   }
 }
 
@@ -84,10 +90,10 @@ function nextIndex() {
   return i
 }
 
-window.addEventListener('resize', resizeCanvas, false)
+window.addEventListener('resize', redrawCanvas, false)
 window.addEventListener('mouseout', mouseOut)
 window.addEventListener("mouseover", mouseIn)
-canvas.addEventListener("contextmenu", disableContextMenu)
+//window.addEventListener("contextmenu", disableContextMenu)
 
 function mouseOut() {
   mouseOutInterval = setInterval(function () {
@@ -103,29 +109,57 @@ function mouseIn() {
   clearInterval(mouseOutInterval)
 }
 
-function resizeCanvas() {
+function redrawCanvas() {
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
+  const width = window.innerWidth
+  const height = window.innerHeight
+  const xGlobal = window.screenX - height/2
+  const yGlobal = window.screenY - width/2
 
-  draw()
-  setCookie(index, positionX + ',' + positionY + ',' + window.innerWidth + ',' +  window.innerHeight)
+  ctx.lineWidth = LINEWIDTH
+  ctx.strokeStyle = colors[index % colors.length]
+  setCookie(index, positionX + ',' + positionY + ',' + width + ',' +  height)
+
+  const radius = 10 + index * 20
+  drawCircle(width/2, height/2, radius)
+  
+  for(const w of windows) {
+    ctx.strokeStyle = colors[w[0] % colors.length]
+    const radius = 10 + w[0] * 20
+    drawCircle(width/2, height/2, radius)
+
+    const x = w[1].split(',')[0]
+    const y = w[1].split(',')[1]
+    const widthWindow = w[1].split(',')[2]
+    const heightWindow = w[1].split(',')[3]
+
+
+    drawLineBetweenEdgeCirle(xGlobal + (x - heightWindow/2 ), yGlobal + (y - widthWindow/2 ), width/2, height/2, radius)
+  }
 }
-      
-function draw() {
-  ctx.lineWidth = 5
-  ctx.strokeStyle = 'blue'
 
+function drawCircle(x, y, radius) {
   ctx.beginPath()
-  ctx.moveTo(window.innerWidth/2, window.innerHeight/2);
-  ctx.lineTo(0,window.innerHeight/3)
+  ctx.arc(x, y,radius, 0, 2 * Math.PI)
   ctx.stroke()
+}
 
+function drawLineBetweenEdgeCirle(xPoint, yPoint, xCircle, yCircle, radius) {
   ctx.beginPath()
-  ctx.arc(window.innerWidth/2, window.innerHeight/2, 20, 0, 2 * Math.PI);
-  ctx.fill();
-  ctx.stroke();
+  ctx.moveTo(xPoint,yPoint)
+  const point = findClosestEdgePoint(xPoint,yPoint, xCircle, yCircle, radius)
+  ctx.lineTo(point[0], point[1])
+  ctx.stroke()
+}
+
+function findClosestEdgePoint(xPoint, yPoint, xCircle, yCircle, radius) {
+  const dx = xPoint - xCircle
+  const dy = yPoint - yCircle
+  const distance = Math.sqrt(dx * dx + dy * dy)
+  return [xCircle + dx * radius / distance, yCircle + dy * radius / distance]
 }
 
 function disableContextMenu(event) {
-  //event.preventDefault();
+  event.preventDefault()
 }
